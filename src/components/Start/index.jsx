ec52c214e9eca3c9a50f107e7cca857e599de2af
Yicel Gutierrez <yicelgutierrezguillen@gmnail.com>
2024-08-27 19:36:07 -0500

    solved import clasp

--- src/components/Start/index.jsx
+++ src/components/Start/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import vector from "../../assets/vector.png";
+import vector from "../../assets/clasp.png";
 
 export const Start = () => {
   return (
